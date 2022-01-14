e469ff95a7957839b02268e6f8ee82db2d091fd9
Nahumus <itaynahum2003@gmail.com>
2022-01-14 19:19:02 +0200

    Fix after debug

--- motorcontrol.ino
+++ motorcontrol.ino
@@ -1,16 +1,16 @@
 // Motor
 const double MOTOR_PIN = 11;
-const double MOTOR_MAX_DIG_DELAY = 5; // Miliseconds
+const double MOTOR_DIG_DELAY = 5; // milliseconds
 const double OUTPUT_MAX_VALUE = 2;
 const double OUTPUT_MIN_VALUE = 1;
 
-// Potentiometer / Controller
+// Potentiometer
 const double POT_PIN = A0;
 const double POT_MAX_VALUE = 1023;
-const double POT_MIN_VALUE = 496;
+const double POT_MIN_VALUE = 0;
 
 // Other
-const double DEADBAND = 0.1; // From 0 to 1
+const double DEADBAND = 0.1; // from 0 to 1
 const bool DEBUG = true;
 
 double potVal;
@@ -28,7 +28,7 @@ void loop()
   potVal = analogRead(POT_PIN);
   outputVal = changeRange(potVal, POT_MIN_VALUE, POT_MAX_VALUE, OUTPUT_MIN_VALUE, OUTPUT_MAX_VALUE);
   outputVal = max(min(outputVal, OUTPUT_MAX_VALUE), OUTPUT_MIN_VALUE);
-  outputVal = addDeadband(potVal, OUTPUT_MIN_VALUE, OUTPUT_MAX_VALUE, DEADBAND);
+  outputVal = addDeadband(outputVal, OUTPUT_MIN_VALUE, OUTPUT_MAX_VALUE, DEADBAND);
 
   if (DEBUG) {
     Serial.print("Potentiometer Value: "); Serial.println(potVal);
@@ -36,21 +36,21 @@ void loop()
   }
 
   digitalWrite(MOTOR_PIN, HIGH);
-  delay(outputVal);
-  
+  delayMicroseconds(outputVal * 1000);
+
   digitalWrite(MOTOR_PIN, LOW);
-  delay(MOTOR_MAX_DIG_DELAY - outputVal);
+  delayMicroseconds((MOTOR_DIG_DELAY - outputVal) * 1000);
 }
 
 
 /**
- * @brief 
- * 
+ * @brief
+ *
  * @param value the current value in the input range
  * @param min min value of the input range
  * @param max max value of the input range
  * @param outputMin
- * @param outputMax 
+ * @param outputMax
  * @return the value relative to the output range
  */
 double changeRange(double value, double min, double max, double outputMin, double outputMax) {
@@ -59,18 +59,18 @@ double changeRange(double value, double min, double max, double outputMin, doubl
 
 
 /**
- * @brief 
- * 
+ * @brief
+ *
  * @param value the input / controller value
  * @param min the minimum value of the range
  * @param max the maximum value of the range
  * @param deadband decimal number from 0 to 1
- * @return the output value after calculating the deadband
+ * @return the output value with the deadband
  */
 double addDeadband(double value, double min, double max, double deadband) {
   if (deadband == 0) return value;
-  deadband = deadband * (max - min);
+  deadband *= max - min;
   double middle = (max + min) / 2.0;
-  return abs(value - (max - min)) <= deadband ? deadband : value > middle ?
+  return abs(value - middle) <= deadband ? middle : value > middle ?
     changeRange(value, middle + deadband, max, middle, max) : changeRange(value, min, middle - deadband, min, middle);
 }
